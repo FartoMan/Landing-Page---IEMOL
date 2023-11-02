18c6492143cfb33df38bc98a1c9f1c213ce98579
Charley Worrison <charleyworrison@gmail.com>
2023-11-02 20:15:05 -0300

    att primeira sessao

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,16 @@
-
+var click_btn_mais = 0;
 $("#btn-mais").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#iemol").offset().top
-    }, 1500);
+    click_btn_mais++;
+    if(click_btn_mais == 1) {
+        $('#home').animate({
+            scrollTop: $("#btn-mais").offset().top
+        }, 1500);
+    }
+    else if (click_btn_mais == 2) {
+        $('html, body').animate({
+            scrollTop: $("#iemol").offset().top
+        }, 1500);
+        click_btn_mais = 0;
+    }
+
 })
