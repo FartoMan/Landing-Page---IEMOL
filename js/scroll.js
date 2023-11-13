a33a0194673aac8f6ced580bc4b7b7e74b419c8e
Charley Worrison <charleyworrison@gmail.com>
2023-11-13 16:14:32 -0300

    finish

--- js/scroll.js
+++ js/scroll.js
@@ -1,17 +1,8 @@
-var click_btn_mais = 0;
+
 $("#btn-mais").click(function() {
-    click_btn_mais++;
-    if(click_btn_mais == 1) {
         $('#home').animate({
             scrollTop: $("#btn-bola-form").offset().top
         }, 1500);
-    }
-    else if (click_btn_mais == 2) {
-        $('html, body').animate({
-            scrollTop: $("#iemol").offset().top
-        }, 1500);
-        click_btn_mais = 0;
-    }
 
 })
 $("#smdiv").click(function() {
